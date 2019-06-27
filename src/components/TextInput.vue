f765da412000077b9eb421da5a00cee35ea91ee1
Maksim <msurguy@gmail.com>
2019-06-26 23:35:15 -0700

    formatting

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -16,15 +16,17 @@
             <h6>Formula Field</h6>
             <p>In this field, <code>i</code> represents the current loop iteration. You can use the following:</p>
             <p>
-            Operators:	<code>+</code>, <code>-</code>, <code>*</code>, <code>/</code>, <code>%</code>, <code>mod</code><br>
-            Arithmetic:	<code>abs</code>, <code>exp</code>, <code>log</code>, <code>sqrt</code>, <code>ceil</code>, <code>floor</code>, <code>random</code>, <code>round</code><br>
-            Trigonometry:	<code>tan</code>, <code>sin</code>, <code>cos</code>, <code>acos</code>, <code>asin</code>, <code>atan</code>, <code>atan2</code><br>
-            Statistics:	<code>max</code>, <code>min</code><br>
-            Constants: <code>pi</code>, <code>e</code></p>
+            Operators:<code>+</code>, <code>-</code>, <code>*</code>, <code>/</code>, <code>%</code>, <code>mod</code><br>
+            Arithmetic:<code>abs</code>, <code>exp</code>, <code>log</code>, <code>sqrt</code>, <code>ceil</code>, <code>floor</code>, <code>random</code>, <code>round</code><br>
+            Trigonometry:<code>tan</code>, <code>sin</code>, <code>cos</code>, <code>acos</code>, <code>asin</code>, <code>atan</code>, <code>atan2</code><br>
+            Statistics:<code>max</code>, <code>min</code><br>
+            Constants:<code>pi</code>, <code>e</code></p>
             <p>Examples:</p>
             <ul class="pl-3">
               <li>
-                <code>i*20 + sin(i)*random()*100</code>
+                <code>
+                  i*20 + sin(i)*random()*100
+                </code>
               </li>
               <li>
                 <code>
