618803412904f272f28303b8facc040a4dd80ff7
Maks Surguy <msurguy@gmail.com>
2020-05-24 15:54:40 -0700

    added analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import './sass/bootstrap.scss'
 import SvgIcon from 'vue-svgicon'
+import VueAnalytics from 'vue-analytics'
 
 export const eventBus = new Vue()
 
@@ -12,6 +13,10 @@ Vue.config.productionTip = false
 // Default tag name is 'svgicon'
 Vue.use(SvgIcon, {
   tagName: 'svgicon'
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-167408902-1'
 })
 
 /* eslint-disable no-new */
